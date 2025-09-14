d8b0e1c0713075d3f471097411c2f18a9441a2a6
sukumar-chennari <chennarisukumar@gmail.com>
2025-09-14 18:18:59 +0530

    middleware

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const express=require('express')
-const {  mainController } = require('./controllers')
 const router = require('./route')
 const PORT=8100
 const app=express()
-app.get('/',mainController)
-// app.get('/user/:username',userController)
-// app.get('/search',queryController)
 
-app.use('/user',router)
+app.use('/users',(req,res,next)=>{
+    console.log(`${req.method} ${req.url} ${new Date()}`);
+    next()
+})
 
-app.post('/users',express.json(),(req,res)=>{
-    const {username,email  }=req.body
-    res.send(`user created ${username} ${email} successfully`)
+app.use('/users',router)
+
+app.get('/',(req,res)=>{
+    res.send('hello buddy')
+})
+
+app.get('/error',(req,res)=>{
+    throw new Error('something went wrong')
+})
+
+app.use((err,req,res,next)=>{
+    console.error(err.stack);
+    res.status(500).send('something broke!')
 })
 
 
-app.put('/users/:id',express.json(),(req,res)=>{
-    const { id } = req.params
-    const {username,email}=req.body
-    res.send(`user ${username} with ${id}  is updated`)
-})
+// app.use('/user',router)
 
 app.listen(PORT,()=>console.log(`server is running on ${PORT}`))
